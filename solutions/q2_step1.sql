5098c2650375037c733718031b8476efef33a344
pablo <pablofelgueres@gmail.com>
2017-04-18 21:31:00 -0700

    Delete q2_step1.sql

--- solutions/q2_step1.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW item_duration AS 
-
-SELECT 
-
-timestampdiff(day, item_sat.period_start_at, item_sat.period_end_at) AS period_duration_days,
-
-proration_ind
-
-FROM v_x_invoice_item_sat AS item_sat
